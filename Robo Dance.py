89a0c0d92e636e6135f0855cbb516b6e6c3619c8
A-Pujols-ASC <apujols17@lawrenceville.org>
2016-07-13 09:26:55 -0400

    1. Fixed "Robot Clapping" but needs syncing 2. Added movement for "To the left" 3. Wrote rough commands for rest of verse but no "cha-cha"

--- Robo Dance.py
+++ Robo Dance.py
@@ -1,13 +1,31 @@
 from Myro import *
-init ("COM*")
-i = 0
-while i < 6:
-beep(.1233333, 900)
-i = i + 1
-for seconds in timer(2): motors(0,1) 
-bacstop()
+Myro.playUntilDone(The Cha Cha Slide - Dj Casper (Lyrics) by Lyndsie15.mp3)
+## i = 0
+## while i < 2:      # Robot "Clapping"
+##     beep(.1233333, 900)
+##     wait(0.7)
+##     beep(.1233333, 900)
+##     wait(0.3)
+##     beep(.1233333, 900)
+##     wait(0.4)
+##     beep(.5, 900)
+##     wait(1.6)
+##     i = i + 1
+for seconds in timer(1):   #To the left
+    rotate(10)
+    stop()
+wait(1)
+backward(3,.3)
+wait(1)
+forward(5,.3)           #one hop this time
+wait(1)
+motors(0,1)
+wait(1)
+motors (1,0)
+wait(1)
+                     #cha chat real smooth
 t = 0
-while t > 2:
+while t < 2:
     backward(10, 1.99)
     turnRight(.1, .3)
     turnLeft(.1, .3)
