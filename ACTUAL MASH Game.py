4b173f2f15ff8a5551a1d4b3f2e806882b760ce3
A-Pujols-ASC <apujols17@lawrenceville.org>
2016-07-16 20:03:58 -0400

    So this is the ACTUAL MASH game script (Not complete) *Added* Intro (for lols) user_input[] to store that input Function that uses input to create a random outcome (not done yet) *Changed*

---
+++ ACTUAL MASH Game.py
@@ -0,0 +1,53 @@
+## from random import *
+## a = randint(1, 4)
+## print (a)
+user_input = []
+## # 0= neutral
+## # 1= good
+## # -1= bad 
+## fate = 0
+name = raw_input("Welcome to the 100% Legit and Scam-Free Gaurenteed Fortune Telling Software (trial version) Please enter your name now")
+creditcard = raw_input("Hello "+name+" Unfortuantely there is no trial version and you must pay now. Enter your credit card number")
+user_input.append(name)
+user_input.append(creditcard)
+print("Thank you for your information! You now have the full version of Fortune Teller!")
+spouse = raw_input("Possible Spouses?")
+kids = raw_input("Number of kids?")
+cars = raw_input("Possible Cars?")
+user_input.append(spouse)
+user_input.append(kids)
+user_input.append(cars)
+randnum = raw_input("Give me a random number to help predict your future!")
+
+## bad_building = ["Cardboard box", "Apartment in North Korea", "Trump Towers (must sleep with Trump", "Barn House"]
+## good_building = ["Trump Towers (you can rename it)", "Bill Gate's Mansion", "Summer House out on Hawaii","Floating Fortress"]
+## bad_job = ["NO JOB FOR YOU! GET INTERESTING!"]
+## good_job = ["Ruler of the World", "CEO of Windows", "Supreme Ruler of the Universe", "God"]
+## bad_mate = ["NO ONE WANTS ANYONE BORING, GET INTERESTING!"]
+## good_mate = ["This Year's Miss America", "Hottest Model in the world", "Anyone", "Your Crush"]
+## bad_offspring = ["NO OFFSPRING FOR YOU SINCE YOU CAN'T GET MATE!"]
+## good_offspring = ["some many children that you could literally make your own country", "The Smartest Kid in the World", "Dat Boi", "Karate Kid"]
+## trig_for_color = ["Red","Blue","Yellow","Green","Purple","Orange"]
+## trig_for_food = ["Pizza","Chicken","Sushi","Steak","Fish","Apple","Orange","Candy"]
+## trig_for_number = ["1","2","3","4","5","6","7","8","9","10","100",]
+## for i in trig_for_color:
+##     if color[i] == color:
+##         fate == -1
+## for i in trig_for_food:
+##     if i == food:
+##         fate == -1
+## for i in trig_for_number:
+##     if i == creditcard:
+##         fate == -1
+## if fate == -1:
+##     print ("You will live in"+bad_building[a]+bad_job+bad_mate+bad_offspring)
+## from random import *
+## randint(x,y)  #Will return a random number between and including x and y
+
+  
+
+## Building
+## Job
+## Mate
+## Offspring
+## Boring answers giving boring outcomes, intersting answers gives interesting/good outcomes)
